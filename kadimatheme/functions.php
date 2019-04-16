9580c4628eb33f09804923742a90fd2d02ebf9e2
souzaramon <ramon_souza@hotmail.com>
2019-04-16 01:36:07 -0300

    [add] Adicionando css base do Tema

--- kadimatheme/functions.php
+++ kadimatheme/functions.php
@@ -1,3 +1,9 @@
 <?php
     require_once get_template_directory() . '/plugins/class-wp-bootstrap-navwalker.php';
     register_nav_menus(array('primary' => __('Primary Menu', 'kadimatheme')));
+
+    function load_assets() {
+        wp_enqueue_style('template', get_template_directory_uri() . '/css/base.css', array(), '1.0', 'all');
+    }
+
+    add_action('wp_enqueue_scripts', 'load_assets');
