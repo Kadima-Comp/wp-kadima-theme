3ae2cfa43c4bf7f803eddee2a05fa887a2038565
souzaramon <ramon_souza@hotmail.com>
2019-04-16 05:40:14 -0300

    [fix] Indentação

--- kadimatheme/footer.php
+++ kadimatheme/footer.php
@@ -1,43 +1,44 @@
-    <footer class="page-footer">
-        <div class="glass">
-            <div class="container-fluid">
-                <div class="row align-items-center">
-                    <div class="container col-10 col-lg-8 col-xl-6">
-                        <h3 class="titulo-footer text-center">
-                            Se cadastre e fique por dentro das novidades!
-                        </h3>
-                        <div class="input-group mb-3">
-                            <input
-                                type="text"
-                                class="capture-email-field form-control"
-                                placeholder="E-mail"
-                                aria-label="E-mail do usuario"
-                                aria-describedby="basic-addon2"
+<footer class="page-footer">
+    <div class="glass">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="container col-10 col-lg-8 col-xl-6">
+                    <h3 class="titulo-footer text-center">
+                        Se cadastre e fique por dentro das novidades!
+                    </h3>
+                    <div class="input-group mb-3">
+                        <input
+                            type="text"
+                            class="capture-email-field form-control"
+                            placeholder="E-mail"
+                            aria-label="E-mail do usuario"
+                            aria-describedby="basic-addon2"
+                        >
+                        <div class="input-group-append">
+                            <button
+                                class="capture-email-button btn btn-secondary"
+                                type="submit"
                             >
-                            <div class="input-group-append">
-                                <button
-                                    class="capture-email-button btn btn-secondary"
-                                    type="submit"
-                                >
-                                    Enviar
-                                </button>
-                            </div>
+                                Enviar
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="container text-left">
-                <div class="row">
-                    <?php dynamic_sidebar( 'footer-1' )?>
-                </div>
-            </div>
-
-            <div class="footer-copyright text-center">
-                <span>&copy; 2019 <?php bloginfo($show = 'name'); ?> - Todos os direitos reservados.</span>
+        <div class="container text-left">
+            <div class="row">
+                <?php dynamic_sidebar( 'footer-1' )?>
             </div>
         </div>
-    </footer>
-    <?php wp_footer(); ?>
+
+        <div class="footer-copyright text-center">
+            <span>&copy; 2019 <?php bloginfo($show = 'name'); ?> - Todos os direitos reservados.</span>
+        </div>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
 </body>
 </html>
