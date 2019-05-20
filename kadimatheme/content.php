c9d4945643347b9456c21b8c29aa0842e9089d87
Toco020 <44736599+Toco020@users.noreply.github.com>
2019-05-20 10:31:15 -0300

    Add files via upload

--- kadimatheme/content.php
+++ kadimatheme/content.php
@@ -1,4 +1,13 @@
 <?php get_header(); ?>
 
+<article>
+    <h2><?php the_title(); ?></h2>
+
+    <p>Por <?php the_autor_posts_link(); ?>, <?php echo get_the_date(); ?></p>
+    <p>Categorias: <?php the_category(' '); ?></p>
+
+    <?php the_content(); ?>
+</article>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
