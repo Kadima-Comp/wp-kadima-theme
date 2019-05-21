0e3ff66da500e4737c4ecaa2218b337c2a546ce7
Toco020 <44736599+Toco020@users.noreply.github.com>
2019-05-21 05:59:55 -0300

    Add files via upload

--- kadimatheme/404.php
+++ kadimatheme/404.php
@@ -1,11 +1,7 @@
 <!-- Já está sendo puxada, só falta ajeitar a página em si -->
-
-<style>
-<?php include 'css/main.css'; ?>
-</style>
 <?php get_header(); ?>
 
-<p>A página não pôde ser encontrada.</p>
+<h2>A página não pôde ser encontrada.</h2>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
