addb2aec84d65dd3ed7372cabdda8bdda9da3bf1
José Carlos <joseisoppo016@gmail.com>
2019-06-19 08:51:21 -0300

    'sidebar_menor'

--- kadimatheme/header.php
+++ kadimatheme/header.php
@@ -18,11 +18,11 @@
             //var mediaini =  ($('section').offset().top)*2;
             //console.log("Media Inicial: ", mediaini);
 
-            //var mediafim = ($('section').innerHeight() - (($('aside#social').innerHeight()*3)/4));
+            var mediafim = ($('section').innerHeight() - (($('aside#social').innerHeight()*3)/5));
             //console.log("Media Final: ", mediafim);
 
             //if (130 <= $(window).scrollTop() && $(window).scrollTop() <= 800) {
-            if (210 < $(window).scrollTop() && $(window).scrollTop() < mediafim) {
+            if (240 < $(window).scrollTop() && $(window).scrollTop() < mediafim) {
                 //alert('Iniciou');
                 document.getElementById('social').style.opacity="1";
                 document.getElementById('social').style.transition=".2s";
